7a666745224e432964567f00921cef6841877d4f
Facundo <facundotarasi@gmail.com>
2022-01-30 18:31:59 -0300

    Modified DataLoader

--- src/modules.py
+++ src/modules.py
@@ -130,6 +130,8 @@ def to_device(data, device):
     """Move tensor(s) to chosen device"""
     if isinstance(data, (list,tuple)):
         return [to_device(x, device) for x in data]
+    elif isinstance(data, dict):
+        return [to_device(data[x], device) for x in data]
     return data.to(device, non_blocking=True)
 
 class DeviceDataLoader():
