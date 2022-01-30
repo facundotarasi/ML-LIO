eb263f6bd78872338bce19ba45efbba90a80f9c9
Facundo <facundotarasi@gmail.com>
2022-01-30 20:11:27 -0300

    Modified DataLoader

--- src/modules.py
+++ src/modules.py
@@ -129,7 +129,7 @@ def to_device(data, device):
     if isinstance(data, (list,tuple)):
         return [to_device(x, device) for x in data]
     elif isinstance(data, dict):
-        return [to_device(data[x], device) for x in data]
+        return {x: to_device(data[x], device) for x in data}
     return data.to(device, non_blocking=True)
 
 class DeviceDataLoader():
