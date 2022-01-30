ec317ca329c163c15e5c3c46fb9ac4e7516e4cd3
Facundo <facundotarasi@gmail.com>
2022-01-30 19:47:19 -0300

    Modified DataLoader

--- src/modules.py
+++ src/modules.py
@@ -117,6 +117,36 @@ def collate_fn(batch):
 
     return tmp
 
+def get_default_device():
+    """Pick GPU if available, else CPU"""
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+    
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    elif isinstance(data, dict):
+        return [to_device(data[x], device) for x in data]
+    return data.to(device, non_blocking=True)
+
+class DeviceDataLoader():
+    """Wrap a dataloader to move data to a device"""
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+        
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for b in self.dl: 
+            yield to_device(b, self.device)
+
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dl)
+
 # Definicion del DataModule
 class DataModule(pl.LightningDataModule):
     def __init__(self, hooks):
@@ -199,16 +229,19 @@ class DataModule(pl.LightningDataModule):
             self.test_ds = self._normalize(test_ds,self.factor_norm)
             
     def train_dataloader(self):
-        return DataLoader(self.train_ds, batch_size=self.batch_size,
-                          shuffle=True, collate_fn=collate_fn, num_workers=2)
+        return DeviceDataLoader(DataLoader(self.train_ds, batch_size=self.batch_size,
+                          shuffle=True, collate_fn=collate_fn, num_workers=2), 
+                          device = get_default_device())
 
     def val_dataloader(self):
-        return DataLoader(self.val_ds, batch_size=self.batch_size,
-                          shuffle=False, collate_fn=collate_fn, num_workers=2)
+        return DeviceDataLoader(DataLoader(self.val_ds, batch_size=self.batch_size,
+                          shuffle=False, collate_fn=collate_fn, num_workers=2),
+                          device = get_default_device())
 
     def test_dataloader(self):
-        return DataLoader(self.test_ds, batch_size=self.batch_size,
-                          shuffle=False, collate_fn=collate_fn, num_workers=2)
+        return DeviceDataLoader(DataLoader(self.test_ds, batch_size=self.batch_size,
+                          shuffle=False, collate_fn=collate_fn, num_workers=2),
+                          device = get_default_device())
 
     def _load_data(self, currstat):
         init = time.time()
